74d53f4b9dbbdd2b5785b7b7d3cea8e3d716bffa
n.shmelyov <shmelev507@gmail.com>
2023-03-05 16:02:23 +0100

    nice try

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,76 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { HOME_PAGE_ENDPOINT } from './endpoints/homepage';
-// import axios from '../axios';
+import { fetchHomePage } from './endpoints/homepage';
+// import { connect } from 'redux';
+
+
+const headerStyle = {
+  background: '#333',
+  color: '#fff',
+  textAlign: 'center',
+  padding: '10px',
+};
+
+const navStyle = {
+  display: 'inline-block',
+  margin: '10px',
+};
+
 
 class App extends React.Component {
+
+  constructor(props){  
+    super(props);  
+    this.state = {  
+         data: null,
+    }
+  }
+
+  componentDidMount = () => {
+    if (!this.state.data) {
+      const data = fetchHomePage();
+      this.setState({data});
+    }
+  }
+
   makeHomePageRequest = async () => {
     // axios.get(HOME_PAGE_ENDPOINT, this.state).then((response) => {
     //   console.log(response.get_json())
     // });
-    try {
-      const response = await fetch(HOME_PAGE_ENDPOINT);
-      const data = await response.json();
-      console.log(data);
-    } catch (error) {
-      console.error(error);
-    }
+    // try {
+    //   const response = await fetch(HOME_PAGE_ENDPOINT);
+    //   const data = await response.json();
+    //   console.log(data);
+    // } catch (error) {
+    //   console.error(error);
+    // }
   };
 
+  renderPageheader = () => {
+    return (
+      <div>
+        <header style={headerStyle}>
+        <h1>My Website Header</h1>
+        <nav style={navStyle}>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+      </div>
+    );
+  }
+
   render() {
+
     return (
       <div className="App">
+        {this.renderPageheader()}
         <header className="App-header">
-          <button onClick={this.makeHomePageRequest}>call</button>
+          <button>call</button>
         </header>
       </div>
     );
--- frontend/src/endpoints/homepage/index.js
+++ frontend/src/endpoints/homepage/index.js
@@ -1,1 +1,27 @@
 export const HOME_PAGE_ENDPOINT = "http://0.0.0.0:8000/";
+
+
+export const fetchHomePage = async () => {
+    try {
+      const response = await fetch(HOME_PAGE_ENDPOINT);
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      return {
+        "data": [
+            {
+                "id": 1,
+                "name": "Nikita",
+            },
+            {
+                "id": 2,
+                "name": "Vova",
+            },
+            {
+                "id": 3,
+                "name": "Glib",
+            }
+        ]
+    }
+    }
+};
