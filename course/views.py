43fc3b3ed5fc341ee7aea8f15041fb00e56fae99
Vladimir Krutko <krutkovova24@gmail.com>
2023-03-05 18:11:53 +0100

    create_topic_endpoint

--- course/views.py
+++ course/views.py
@@ -3,10 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Course, CourseTopic
 from .serializers import CourseSerializer, TopicSerializer
+from django.db.models import Max
 
 class CourseListApiView(APIView):
     def get(self, request, *args, **kwargs):
-
         courses = Course.objects.filter(status='O')
         serializer = CourseSerializer(courses, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -21,22 +21,24 @@ class CourseListApiView(APIView):
         serializer = CourseSerializer(data=data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response({'data': serializer.data}, status=status.HTTP_201_CREATED)
 
         return Response({'data': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 class TopicLiatApiView(APIView):
+    
     def get(self, request, pk=None, *args, **kwargs):
-        topics = CourseTopic.objects(pk=pk)
-        serializer = TopicSerializer(topics)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        topics = CourseTopic.objects.filter(course_id=pk)
+        serializer = TopicSerializer(topics, many=True)
+        
+        return Response({'data': serializer.data}, status=status.HTTP_200_OK)
     
     def post(self, request, pk=None, *args, **kwargs):
-        
+        last_topics = int(CourseTopic.objects.filter(course_id=pk).aggregate(Max('course_position')).course_position or 0)
         data = {
             'name': request.data.get('name'), 
-            'course_id': pk, 
-            'summary': -1
+            'course_id': Course.objects.get(pk=pk), 
+            'course_position': last_topics+1
         }
         serializer = CourseSerializer(data=data)
         if serializer.is_valid():
@@ -44,4 +46,5 @@ class TopicLiatApiView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
         
